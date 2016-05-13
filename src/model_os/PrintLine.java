1721c706c6c49b0ea6e5031ad7ce5bf0bdb6aef2
Agne Pelurityte <agnepe@gmail.com>
2016-05-13 15:24:35 +0300

    Fixes

--- src/model_os/PrintLine.java
+++ src/model_os/PrintLine.java
@@ -1,30 +1,32 @@
 package model_os;
 
+import java.util.List;
+
 /**
  * Created by NekoChan on 2016-05-13.
  */
 public class PrintLine extends TProcess {
 
-    public PrintLine (TKernel kernel, TPState pState, TPRocess pParent,
-                      int pPriority, List<Telements> pORElements) {
+    public PrintLine (TKernel kernel, TPState pState, TProcess pParent,
+                      int pPriority, List<TElement> pORElements) {
         super(kernel, pState, pParent, pPriority, pORElements);
     }
 
     public void phase1 () {
         phase = 2;
-        kernel.requestResource(this, ResourceClass.LINETOPRINT, 0);
+        kernel.askForResource(this, TResource.ResourceClass.LINETOPRINT, 0);
     }
 
     public void phase2 () {
         phase = 3;
-        kernel.requestResource(this, resourceClass.CHANNELDEVICE, 0);
+        kernel.askForResource(this, TResource.ResourceClass.CHANNELDEVICE, 0);
     }
 
     public void phase3 () throws Exception {
         phase = 1;
-        TElement lineToPrint = getElement (ResourceClass.LINETOPRINT);
+        TElement lineToPrint = getElement (TResource.ResourceClass.LINETOPRINT);
         kernel.print(lineToPrint.getInfo());
-        TElement channelDevice = getElement (ResourceClass.CHANNELDEVICE);
-        kernel.releaseResource (ResourceClass.CHANNELDEVICE, channelDevice);
+        TElement channelDevice = getElement (TResource.ResourceClass.CHANNELDEVICE);
+        kernel.freeResource (TResource.ResourceClass.CHANNELDEVICE, channelDevice);
     }
 }
--- src/model_os/UploadProgram.java
+++ src/model_os/UploadProgram.java
@@ -1,5 +1,7 @@
 package model_os;
 
+import java.util.List;
+
 /**
  * Created by NekoChan on 2016-05-13.
  */
@@ -12,22 +14,22 @@ public class UploadProgram extends TProcess {
 
     public void phase1 () {
         phase = 2;
-        kernel.requestResource(this, ResourceClass.LOADPROGRAM, 0);
+        kernel.askForResource(this, TResource.ResourceClass.LOADPROGRAM, 0);
     }
 
     public void phase2 () {
         phase = 3;
-        kernel.requestResource(this, ResourceClass.GENERALMEMORY, 0);
+        kernel.askForResource(this, TResource.ResourceClass.GENERALMEMORY, 0);
     }
 
     public void phase3 () {
         phase = 4;
-        kernel.requestResource(this, ResourceClass.CHANNELDEVICE, 0);
+        kernel.askForResource(this, TResource.ResourceClass.CHANNELDEVICE, 0);
     }
 
     public void phase4 () throws Exception {
         phase = 5;
-        TElement loadProgram = getElement (ResourceClass.LOADPROGRAM);
+        TElement loadProgram = getElement (TResource.ResourceClass.LOADPROGRAM);
         String [] addresses = loadProgram.getInfo().split(":");
         int start = Integer.valueOf(addresses[0]);
         int end = Integer.valueOf(addresses[1]);
@@ -35,12 +37,12 @@ public class UploadProgram extends TProcess {
         for (int i = start; i < end; i++) {
             kernel.getGeneralMemory()[gmIdx++] = kernel.getHdd().getMemory(i / 10, i % 10);
         }
-        TElements channelDevice = getElement (ResourceClass.CHANNELDEVICE);
-        kernel.releaseResource (ResourceClass.CHANNELDEVICE, channelDevice);
+        TElement channelDevice = getElement (TResource.ResourceClass.CHANNELDEVICE);
+        kernel.freeResource (TResource.ResourceClass.CHANNELDEVICE, channelDevice);
     }
 
     public void phase5 () {
         phase = 1;
-        kernel.releaseResource(ResourceClass.LOADEDPROGRAM, new TElement (null, this, null));
+        kernel.freeResource(TResource.ResourceClass.LOADEDPROGRAM, new TElement (null, this, null));
     }
 }
